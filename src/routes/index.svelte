e63e63e95d8260333dd146d24eee01f09d08b6db
Anna Aneychik <anna.aneychik@ridedott.com>
2021-08-18 22:44:54 +0200

    Server-side render an example string

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,25 @@
+<script context="module" lang="ts">
+	import type { Load } from '@sveltejs/kit';
+
+	export const load: Load = async ({ fetch }) => {
+		const response = await fetch('/api/sheet');
+
+		if (response.ok) {
+			const { sheet } = await response.json();
+			return { props: { sheet } };
+		}
+
+		return {
+			status: response.status,
+			error: new Error('Could not load document')
+		};
+	};
+</script>
+
+<script>
+	export let sheet;
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<p>Sheet loaded: {sheet}</p>
